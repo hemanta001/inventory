4b801fb414ef2b682cbcb43285d25bfec9126f03
hemanta001 <rockingguyheman.hg@gmail.com>
2018-09-12 15:18:58 +0545

    new

--- grails-app/controllers/LoginController.groovy
+++ grails-app/controllers/LoginController.groovy
@@ -1,4 +1,9 @@
-
+import grails.converters.JSON
+import grails.converters.XML
+import groovy.json.JsonSlurper
+import org.codehaus.groovy.grails.web.json.JSONArray
+import org.codehaus.groovy.grails.web.json.JSONObject
+import org.codehaus.groovy.grails.web.json.parser.JSONParser
 
 class LoginController {
 
@@ -35,5 +40,61 @@ if(session.adminUser){
             render(view: "/home/error500")
         }
     }
+
+//def check(){
+//    String inliness=""
+//    URL url = new URL("http://localhost:8081/rest/book/list?username=hemanta")
+//    HttpURLConnection conn = (HttpURLConnection)url.openConnection()
+//    conn.setRequestMethod("GET")
+//    int responsecode = conn.getResponseCode()
+//    if(responsecode != 200) {
+//        render(view: "check")
+//    }
+//        else
+//
+//    {
+//        Scanner sc = new Scanner(url.openStream());
+//        while(sc.hasNext())
+//        {
+//            inliness+=sc.nextLine();
+//        }
+//        System.out.println("\nJSON data in string format")
+//        System.out.println(inliness)
+//        sc.close()
+//        def jsonSlurper = new JsonSlurper()
+//        def object = jsonSlurper.parseText(inliness)
+//        [object:object]
+//    }
+//
+//}
+//    def list() {
+//        if(params.username=="hemanta"){
+//            def books = Book.list()
+//
+//            withFormat {
+//
+//                json { render books as JSON }
+//                xml { render books as XML }
+//            }
+//
+//        }
+//
+//    }
+//
+//    def show(Integer id) {
+//        def book = Book.get(id)
+//        if (book) {
+//            withFormat {
+//                xml {
+//                    render book as XML
+//                }
+//                json {
+//                    render book as JSON
+//                }
+//            }
+//        } else {
+//            render "book not found"
+//        }
+//    }
 
 }
