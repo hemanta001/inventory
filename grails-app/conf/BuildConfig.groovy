945dfc2b53c5102610604998747e3591793b2e99
hemanta001 <rockingguyheman.hg@gmail.com>
2018-09-14 16:02:10 +0545

    new

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -53,9 +53,11 @@ grails.project.dependency.resolution = {
 
         // runtime 'org.postgresql:postgresql:9.3-1101-jdbc41'
         test "org.grails:grails-datastore-test-support:1.0.2-grails-2.4"
+
     }
 
     plugins {
+
         // plugins for the build system only
         build ":tomcat:7.0.55.2" // or ":tomcat:8.0.20"
 
